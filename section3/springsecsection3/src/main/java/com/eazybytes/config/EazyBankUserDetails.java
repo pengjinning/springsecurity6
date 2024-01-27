73994c0b0559709138c3b8992bdc44a74575516e
jack ning <github@bytedesk.com>
2024-01-27 16:21:19 +0800

    update

--- section3/springsecsection3/src/main/java/com/eazybytes/config/EazyBankUserDetails.java
+++ section3/springsecsection3/src/main/java/com/eazybytes/config/EazyBankUserDetails.java
@@ -1,5 +1,20 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2024-01-27 13:47:16
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2024-01-27 15:35:54
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  仅支持企业内部员工自用，严禁用于销售、二次销售或者部署SaaS方式销售 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ *  技术/商务联系：270580156@qq.com
+ * Copyright (c) 2024 by bytedesk.com, All Rights Reserved. 
+ */
 package com.eazybytes.config;
 
+// 
 import com.eazybytes.model.Customer;
 import com.eazybytes.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,13 +42,13 @@ public class EazyBankUserDetails implements UserDetailsService {
         List<Customer> customer = customerRepository.findByEmail(username);
         if (customer.size() == 0) {
             throw new UsernameNotFoundException("User details not found for the user : " + username);
-        } else{
+        } else {
             userName = customer.get(0).getEmail();
             password = customer.get(0).getPwd();
             authorities = new ArrayList<>();
             authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
         }
-        return new User(userName,password,authorities);
+        return new User(userName, password, authorities);
     }
 
 }
--- section3/springsecsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ section3/springsecsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -1,19 +1,26 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2024-01-27 13:47:16
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2024-01-27 14:32:13
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  仅支持企业内部员工自用，严禁用于销售、二次销售或者部署SaaS方式销售 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ *  技术/商务联系：270580156@qq.com
+ * Copyright (c) 2024 by bytedesk.com, All Rights Reserved. 
+ */
 package com.eazybytes.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -21,9 +28,9 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf((csrf) -> csrf.disable())
-                .authorizeHttpRequests((requests)->requests
-                        .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
-                        .requestMatchers("/notices","/contact","/register").permitAll())
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                        .requestMatchers("/notices", "/contact", "/register").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
@@ -40,39 +47,49 @@ public class ProjectSecurityConfig {
         return NoOpPasswordEncoder.getInstance();
     }
 
-    /*@Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        *//*Approach 1 where we use withDefaultPasswordEncoder() method
-		while creating the user details*//*
-        *//*UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("12345")
-                .authorities("admin")
-                .build();
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("12345")
-                .authorities("read")
-                .build();
-        return new InMemoryUserDetailsManager(admin, user);*//*
+    /*
+     * @Bean
+     * public InMemoryUserDetailsManager userDetailsService() {
+     *//*
+        * Approach 1 where we use withDefaultPasswordEncoder() method
+        * while creating the user details
+        *//*
+          *//*
+              * UserDetails admin = User.withDefaultPasswordEncoder()
+              * .username("admin")
+              * .password("12345")
+              * .authorities("admin")
+              * .build();
+              * UserDetails user = User.withDefaultPasswordEncoder()
+              * .username("user")
+              * .password("12345")
+              * .authorities("read")
+              * .build();
+              * return new InMemoryUserDetailsManager(admin, user);
+              *//*
+                
+                *//*
+                    * Approach 2 where we use NoOpPasswordEncoder Bean
+                    * while creating the user details
+                    *//*
+                       * UserDetails admin = User.withUsername("admin")
+                       * .password("12345")
+                       * .authorities("admin")
+                       * .build();
+                       * UserDetails user = User.withUsername("user")
+                       * .password("12345")
+                       * .authorities("read")
+                       * .build();
+                       * return new InMemoryUserDetailsManager(admin, user);
+                       * 
+                       * }
+                       */
 
-         *//*Approach 2 where we use NoOpPasswordEncoder Bean
-		while creating the user details*//*
-        UserDetails admin = User.withUsername("admin")
-                .password("12345")
-                .authorities("admin")
-                .build();
-        UserDetails user = User.withUsername("user")
-                .password("12345")
-                .authorities("read")
-                .build();
-        return new InMemoryUserDetailsManager(admin, user);
-
-    }*/
-
-    /*@Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
-    }*/
+    /*
+     * @Bean
+     * public UserDetailsService userDetailsService(DataSource dataSource) {
+     * return new JdbcUserDetailsManager(dataSource);
+     * }
+     */
 
 }
